4516b1e97a6131c9e2cd8b6d31a71c15ae60fab7
captivea-ika <isufi.kapasi@aktivsoftware.com>
2020-02-28 17:32:51 +0530

    [FIX] Validation for input type and done button mail

--- cap_web_free_quote/static/src/js/estimate_insurance.js
+++ cap_web_free_quote/static/src/js/estimate_insurance.js
@@ -3,77 +3,93 @@ odoo.define('cap_web_free_quote.estimate_insurance', function (require) {
     var ajax = require('web.ajax');
 
     $(document).ready(function () {
-        //jQuery time
-        var current_fs; //fieldsets
-        $(".estimate_next").click(function(){
-            var self = $(this);
-            var store_data_model = self.attr('data-store-model');
-            if(store_data_model){
-                current_fs = self.parent();
-                var element = '';
-                var input_args = {};
-                var inp_data = '';
-                current_fs.find('input').each(function(){
-                    element = $(this);
-                    inp_data = element.val();
-                    if(element.hasClass('input-date-save')) {
-                        inp_data = inp_data.split("/");
-                        inp_data = inp_data[2].trim() + '-' + inp_data[0].trim() + '-' + inp_data[1].trim();
-                    }
-                    input_args[element.context['name']] = inp_data;
-                });
-                current_fs.find('select').each(function(){
-                    input_args[$(this).context['name']] = $(this).val();
-                });
-                current_fs.find('textarea').each(function(){
-                    input_args[$(this).context['name']] = $(this).val();
-                });
-                var rec_id = $('#msform').find("input[name='res_id']").val();
-                var return_val = self.attr('data-return');
-                var route = self.attr('data-route');
-                var form  = $('#msform').parent();
-                ajax.jsonRpc(route, 'call', {
-                    'input_args': input_args,
-                    'rec_id': rec_id,
-                    'store_data_model': store_data_model,
-                    'return_val': return_val
-                }).then(function (data) {
-                    if(data['create']) {
-                        $('#msform').find("input[name='res_id']").val(data['res_id']);
-                    }
-                    if(data['ref_code']){
-                        $('#msform').find("input[name='ref_code']").val(data['ref_code']);
-                    }
-                    if(return_val == 'total_insurance') {
-                        form.next().find('.total_insurance')
-                        .html(data['total_insurance']
-                        .toLocaleString('en-EN', {
-                            style: 'currency',
-                            currency: 'USD'
-                        }));
-                    }
-                });
-            }
-        });
 
-        $(".submit_estimate").click(function() {
-            var self = $(this);
-            var rec_id = $('#msform').find("input[name='res_id']").val();
-            var store_data_model = self.attr('data-store-model');
-            var route = self.attr('data-route-done');
-            ajax.jsonRpc(route, 'call', {
-                'rec_id': rec_id,
-                'store_data_model': store_data_model,
-            }).then(function (data) {
-                if(data){
-                    if('total_insurance' in data){
-                        $('#msform').parent().next()
-                        .find('.insurance_amount').html(data['total_insurance']
-                        .toLocaleString('en-EN', {style: 'currency', currency: 'USD'}));
-                    }
-                }
-            });
-        });
+//        //jQuery time
+//        var current_fs; //fieldsets
+//        $(".estimate_next").click(function(){
+//            var self = $(this);
+//            var store_data_model = self.attr('data-store-model');
+//            if(store_data_model){
+//                current_fs = self.parent();
+//                var element = '';
+//                var input_args = {};
+//                var inp_data = '';
+//                current_fs.find('input').each(function(){
+//                    element = $(this);
+//                    inp_data = element.val();
+//                    if(element.hasClass('input-date-save')) {
+//                        inp_data = inp_data.split("/");
+//                        inp_data = inp_data[2].trim() + '-' + inp_data[0].trim() + '-' + inp_data[1].trim();
+//                    }
+//                    input_args[element.context['name']] = inp_data;
+//                });
+//                current_fs.find('select').each(function(){
+//                    input_args[$(this).context['name']] = $(this).val();
+//                });
+//                current_fs.find('textarea').each(function(){
+//                    input_args[$(this).context['name']] = $(this).val();
+//                });
+//                var rec_id = $('#msform').find("input[name='res_id']").val();
+//                var return_val = self.attr('data-return');
+//                var route = self.attr('data-route');
+//                var form  = $('#msform').parent();
+//                ajax.jsonRpc(route, 'call', {
+//                    'input_args': input_args,
+//                    'rec_id': rec_id,
+//                    'store_data_model': store_data_model,
+//                    'return_val': return_val
+//                }).then(function (data) {
+//                    if(data['create']) {
+//                        $('#msform').find("input[name='res_id']").val(data['res_id']);
+//                    }
+//                    if(data['ref_code']){
+//                        $('#msform').find("input[name='ref_code']").val(data['ref_code']);
+//                    }
+//                    if(return_val == 'total_insurance') {
+//                        form.next().find('.total_insurance')
+//                        .html(data['total_insurance']
+//                        .toLocaleString('en-EN', {
+//                            style: 'currency',
+//                            currency: 'USD'
+//                        }));
+//                    }
+//                    route = self.attr('data-route-done');
+//                    if(route){
+//                        ajax.jsonRpc(route, 'call', {
+//                            'rec_id': rec_id,
+//                            'store_data_model': store_data_model
+//                        }).then(function (data) {
+//                            if(data){
+//                                if('total_insurance' in data) {
+//                                    $('#msform').parent().next()
+//                                    .find('.insurance_amount').html(data['total_insurance']
+//                                    .toLocaleString('en-EN', {style: 'currency', currency: 'USD'}));
+//                                }
+//                            }
+//                        });
+//                    }
+//                });
+//            }
+//        });
+
+//        $(".submit_estimate").click(function() {
+//            var self = $(this);
+//            var rec_id = $('#msform').find("input[name='res_id']").val();
+//            var store_data_model = self.attr('data-store-model');
+//            var route = self.attr('data-route-done');
+//            ajax.jsonRpc(route, 'call', {
+//                'rec_id': rec_id,
+//                'store_data_model': store_data_model,
+//            }).then(function (data) {
+//                if(data){
+//                    if('total_insurance' in data){
+//                        $('#msform').parent().next()
+//                        .find('.insurance_amount').html(data['total_insurance']
+//                        .toLocaleString('en-EN', {style: 'currency', currency: 'USD'}));
+//                    }
+//                }
+//            });
+//        });
 
         $("#insurance_done").on('click', function(e){
             var self = $(this);
--- cap_web_free_quote/static/src/js/multiform.js
+++ cap_web_free_quote/static/src/js/multiform.js
@@ -1,6 +1,8 @@
 odoo.define('cap_web_free_quote.free_quote', function (require){
     "use strict";
 
+    var ajax = require('web.ajax');
+
     $(document).ready(function (){
         //jQuery time
         var current_fs, next_fs, previous_fs; //fieldsets
@@ -50,12 +52,14 @@ odoo.define('cap_web_free_quote.free_quote', function (require){
             var self = $(this);
             if(animating) return false;
             animating = true;
-            var missed_required_input = false;
+            var all_required_input = true;
+            var all_required_textarea = true;
+            var all_required_select = true;
             current_fs = self.closest('fieldset');
             next_fs = self.closest('fieldset').next();
             current_fs.find('input').each(function(){
                 if($(this).prop('required') && !$(this).val().trim()){
-                    missed_required_input = true;
+                    all_required_input = false;
                     animating = false;
                     if(!$(this).hasClass('fill-required')){
                         $(this).addClass('fill-required');
@@ -66,37 +70,34 @@ odoo.define('cap_web_free_quote.free_quote', function (require){
                     }
                 }
             });
-            if(!missed_required_input) {
-                current_fs.find('textarea').each(function(){
-                    if($(this).prop('required') && !$(this).val().trim()){
-                        missed_required_input = true;
-                        animating = false;
-                        if(!$(this).hasClass('fill-required')){
-                            $(this).addClass('fill-required');
-                        }
-                    } else {
-                        if($(this).hasClass('fill-required')){
-                            $(this).removeClass('fill-required');
-                        }
+            current_fs.find('textarea').each(function(){
+                if($(this).prop('required') && !$(this).val().trim()){
+                    all_required_textarea = false;
+                    animating = false;
+                    if(!$(this).hasClass('fill-required')){
+                        $(this).addClass('fill-required');
                     }
-                });
-            }
-            if(!missed_required_input) {
-                current_fs.find('select').each(function(){
-                    if($(this).prop('required') && !$(this).val().trim()){
-                        missed_required_input = true;
-                        animating = false;
-                        if(!$(this).hasClass('fill-required')){
-                            $(this).addClass('fill-required');
-                        }
-                    } else {
-                        if($(this).hasClass('fill-required')){
-                            $(this).removeClass('fill-required');
-                        }
+                } else {
+                    if($(this).hasClass('fill-required')){
+                        $(this).removeClass('fill-required');
                     }
-                });
-            }
-            if(!missed_required_input){
+                }
+            });
+            current_fs.find('select').each(function(){
+                if($(this).prop('required') && !$(this).val().trim()){
+                    all_required_select = false;
+                    animating = false;
+                    if(!$(this).hasClass('fill-required')){
+                        $(this).addClass('fill-required');
+                    }
+                } else {
+                    if($(this).hasClass('fill-required')){
+                        $(this).removeClass('fill-required');
+                    }
+                }
+            });
+
+            if(all_required_input && all_required_textarea && all_required_select){
                 //activate next step on progressbar using the index of next_fs
                 $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
                 //show the next fieldset
@@ -127,6 +128,68 @@ odoo.define('cap_web_free_quote.free_quote', function (require){
                 });
                 if(self.hasClass('show_total')) {
                     $('#msform').parent().next().css("display", "block");
+                }
+
+                var store_data_model = self.attr('data-store-model');
+                if(store_data_model) {
+                    var element = '';
+                    var input_args = {};
+                    var inp_data = '';
+                    current_fs.find('input').each(function() {
+                        element = $(this);
+                        inp_data = element.val();
+                        if(element.hasClass('input-date-save')) {
+                            inp_data = inp_data.split("/");
+                            inp_data = inp_data[2].trim() + '-' + inp_data[0].trim() + '-' + inp_data[1].trim();
+                        }
+                        input_args[element.context['name']] = inp_data;
+                    });
+                    current_fs.find('select').each(function() {
+                        input_args[$(this).context['name']] = $(this).val();
+                    });
+                    current_fs.find('textarea').each(function() {
+                        input_args[$(this).context['name']] = $(this).val();
+                    });
+                    var rec_id = $('#msform').find("input[name='res_id']").val();
+                    var return_val = self.attr('data-return');
+                    var route = self.attr('data-route');
+                    var form  = $('#msform').parent();
+                    ajax.jsonRpc(route, 'call', {
+                        'input_args': input_args,
+                        'rec_id': rec_id,
+                        'store_data_model': store_data_model,
+                        'return_val': return_val
+                    }).then(function (data) {
+                        if(data['create']) {
+                            $('#msform').find("input[name='res_id']").val(data['res_id']);
+                        }
+                        if(data['ref_code']) {
+                            $('#msform').find("input[name='ref_code']").val(data['ref_code']);
+                        }
+                        if(return_val == 'total_insurance') {
+                            form.next().find('.total_insurance')
+                            .html(data['total_insurance']
+                            .toLocaleString('en-EN', {
+                                style: 'currency',
+                                currency: 'USD'
+                            }));
+                        }
+                        route = self.attr('data-route-done');
+                        if(route) {
+                            ajax.jsonRpc(route, 'call', {
+                                'rec_id': rec_id,
+                                'store_data_model': store_data_model
+                            }).then(function (data) {
+                                if(data) {
+                                    if('total_insurance' in data) {
+                                        $('#msform').parent().next()
+                                            .find('.insurance_amount').html(data['total_insurance']
+                                            .toLocaleString('en-EN', {style: 'currency', currency: 'USD'}));
+                                    }
+                                }
+                            });
+                        }
+                    });
                 }
             }
         });
@@ -168,5 +231,21 @@ odoo.define('cap_web_free_quote.free_quote', function (require){
                 $('#msform').parent().next().css("display", "none")
             }
         });
+
+        $('.addStar').click(function(ths,sno) {
+            for(var i = 1; i <= 5; i++) {
+                var cur = document.getElementById("star" + i);
+                cur.className = "fa fa-star";
+            }
+            for(var i = 1; i <= sno; i++) {
+                var cur = document.getElementById("star" + i)
+                if(cur.className == "fa fa-star") {
+                    cur.className = "fa fa-star checked"
+                }
+            }
+            $(this).parent().next().val(sno);
+        });
+
+
     });
 });
