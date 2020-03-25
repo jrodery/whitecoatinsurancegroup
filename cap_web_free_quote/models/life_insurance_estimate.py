28a30d8fb7dd77c383a665fc19a09fa418106bdb
NathanQj <nathan.qjm@gmail.com>
2020-03-24 23:38:12 -0400

    fix Total annual Recurring expenses

--- cap_web_free_quote/models/life_insurance_estimate.py
+++ cap_web_free_quote/models/life_insurance_estimate.py
@@ -176,10 +176,7 @@ class LifeInsuranceEstimate(models.Model):
     def _compute_annual_recurring_expenses(self):
         for rec in self:
             amount = rec.question_24_1 + rec.question_26 * 12 + rec.question_27 * 12 - rec.question_5_3
-            if rec.question_4:
-                rec.question_27_1 = amount - rec.question_4
-            else:
-                rec.question_27_1 = amount
+            rec.question_27_1 = amount
             # rec.question_27_2 = amount * rec.remaining_age
             rec.question_27_3 = amount / 0.05
 
