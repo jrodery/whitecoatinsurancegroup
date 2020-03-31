cc73cac27cf17b90bbda3ca212697825794e5798
NathanQj <nathan.qjm@gmail.com>
2020-03-31 12:04:01 -0400

    update form and js

--- cap_web_free_quote/static/src/js/estimate_insurance.js
+++ cap_web_free_quote/static/src/js/estimate_insurance.js
@@ -5,49 +5,33 @@ odoo.define('cap_web_free_quote.estimate_insurance', function (require) {
     $(document).ready(function () {
 
         $(function() {
+          var $input = $('#requested_benefit');
 
-           var $form = $( "#form" );
-           var $input = $form.find( "input" );
-
-           $input.on( "keyup", function( event ) {
-
+           $input.on("keyup", function(event) {
 
                // When user select text in the document, also abort.
                var selection = window.getSelection().toString();
-               if ( selection !== '' ) {
+               if (selection !== '') {
                    return;
                }
-               var $this = $( this );
+
+               // When the arrow keys are pressed, abort.
+               if ($.inArray(event.keyCode, [38,40,37,39]) !== -1) {
+                   return;
+               }
+
+               var $this = $(this);
+
                // Get the value.
                var input = $this.val();
 
                var input = input.replace(/[\D\s\._\-]+/g, "");
-                       input = input ? parseInt( input, 10 ) : 0;
+                       input = input ? parseInt(input, 10) : 0;
 
-                       $this.val( function() {
-                           return ( input === 0 ) ? "" : input.toLocaleString( "en-US" );
-                       } );
-           } );
-
-           /**
-            * ==================================
-            * When Form Submitted
-            * ==================================
-            */
-           $form.on( "submit", function( event ) {
-
-               var $this = $( this );
-               var arr = $this.serializeArray();
-
-               for (var i = 0; i < arr.length; i++) {
-                       arr[i].value = arr[i].value.replace(/[($)\s\._\-]+/g, ''); // Sanitize the values.
-               };
-
-               console.log( arr );
-
-               event.preventDefault();
+               $this.val(function() {
+                   return (input === 0) ? "" : input.toLocaleString("en-US");
+               });
            });
-
          });
 
 
